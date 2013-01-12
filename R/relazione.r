dfecae42231f7492aadcd255329fe742855d5fee
Arguggi <Arguggi@gmail.com>
2013-01-12 08:39:04 +0100

    Data.frame delle medie divisi per CDL

--- R/relazione.r
+++ R/relazione.r
@@ -10,11 +10,14 @@ nomi_test <- colnames(dati[,3:7])
 # Divido il data.frame per corso di laurea
 divisi <- split(dati,dati$CDL)
 
+# Creo il data frame con le medie diviso per CDL
 for (i in divisi) {
   if(i$CDL[1] == "AMMINISTRAZIONE_E_CONTROLLO") {
-    divisi_medie <- data.frame(apply(i[,3:7],2,mean))
+    divisi_medie <- matrix(colMeans(i[,3:7]),nrow=1)
   }
   else {
+    divisi_medie <- rbind(divisi_medie,colMeans(i[,3:7]))
   }  
 } 
 
+divisi_medie <- as.data.frame(divisi_medie)
